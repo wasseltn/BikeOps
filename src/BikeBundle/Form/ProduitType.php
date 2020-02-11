53d338c56ee2a26d69d3f490fb4570f1e2ac3802
benaissasouhaiel <benaissasouhaiel@gmail.com>
2020-02-11 21:58:41 +0100

    Crud Produit +CRUD Catégorie

---
+++ src/BikeBundle/Form/ProduitType.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace BikeBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class ProduitType extends AbstractType
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder->add('name')->add('description')->add('stock')->add('prix')->add('categorie');
+    }/**
+     * {@inheritdoc}
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'BikeBundle\Entity\Produit'
+        ));
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getBlockPrefix()
+    {
+        return 'bikebundle_produit';
+    }
+
+
+}
