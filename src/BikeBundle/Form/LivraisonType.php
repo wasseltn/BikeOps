8d86d1634dbd2f4b5c9b3ac63cbe1ec1fc71ff5f
Ghaylen <60819539+Ghaylen@users.noreply.github.com>
2020-02-16 00:48:37 +0100

    crud livreur + minor changes

--- src/BikeBundle/Form/LivraisonType.php
+++ src/BikeBundle/Form/LivraisonType.php
@@ -2,6 +2,7 @@
 
 namespace BikeBundle\Form;
 
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
@@ -13,7 +14,7 @@ class LivraisonType extends AbstractType
      */
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
-        $builder->add('etat')->add('adresse')->add('utilisateur')->add('commande');
+        $builder->add('etat')->add('adresse')->add('Livreur_id',EntityType::class,array('class'=>'BikeBundle:Livreur','choice_label'=>'nom'));
     }/**
      * {@inheritdoc}
      */
